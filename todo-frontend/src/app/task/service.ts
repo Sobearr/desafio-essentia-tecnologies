29cffb713898a1190117d142085d286ce67bf10e
Victor Schlickmann Sobierajski <victorsobierajski@gmail.com>
2025-07-18 19:48:08 -0300

    feat: criar service para pegar as Tasks do DB

---
+++ todo-frontend/src/app/task/service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Task } from './model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private baseUrl = 'http://localhost:3000/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.baseUrl);
+  }
+}
